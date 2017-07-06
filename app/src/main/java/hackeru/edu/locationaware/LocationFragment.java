386fe4ea541816a4e10be5605a10014cb22a821e
hackeru <hackerudroids@gmail.com>
2017-07-06 20:39:02 +0300

    location request

--- app/src/main/java/hackeru/edu/locationaware/LocationFragment.java
+++ app/src/main/java/hackeru/edu/locationaware/LocationFragment.java
@@ -1,11 +1,26 @@
 package hackeru.edu.locationaware;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 
 /**
@@ -19,11 +34,87 @@ import android.view.ViewGroup;
  */
 public class LocationFragment extends Fragment {
 
+    @BindView(R.id.tvLocation)
+    TextView tvLocation;
+    Unbinder unbinder;
+    //Api provider
+    FusedLocationProviderClient client;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false);
+        View view = inflater.inflate(R.layout.fragment_location, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+        client = new FusedLocationProviderClient(getContext());
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getLastKnownLocation();
+    }
+
+    //Might be null especially in the emulator:
+    private void getLastKnownLocation() {
+        if (!checkLocationPermission()) return;
+
+        Task<Location> task = client.getLastLocation();
+
+        task.addOnCompleteListener(getActivity(), new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful()) {
+                    Location location = task.getResult();
+                    if (location != null) {
+                        tvLocation.setText(location.toString());
+                        tvLocation.setTextSize(20);
+                    }
+                }
+            }
+        });
+    }
+
+    private boolean checkLocationPermission() {
+        //if no permission -> request it and get out.
+        if (ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(
+                    getActivity()/*Activity*/,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1
+            );
+            return false;
+        }
+        return true;
+    }
+
+    private void getLocationUpdates() {
+        LocationRequest request = new LocationRequest();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);//GPS
+        /*request.setPriority(LocationRequest.PRIORITY_LOW_POWER);//Cellular
+        request.setPriority(LocationRequest.PRIORITY_NO_POWER);//Last Known Location
+        request.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);//GPS + Cellular */
+
+        request.setInterval(30 * 1000); //check the gps chip each 30 seconds
+        request.setFastestInterval(500);
+
+        //request.setNumUpdates //infinite updates
+        //request.setExpirationDuration(60*60*1000);//stop after one hour
+        //request.setSmallestDisplacement(1000);//in meters
+
+
+        //client.requestLocationUpdates()
     }
 
 }
